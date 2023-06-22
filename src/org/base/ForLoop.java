32730309c080a613f75d69b5f0378ab771ab6b92
RagulRA130 <ragulraragulra@gmail.com>
2023-06-22 11:32:13 -0700

    message

---
+++ src/org/base/ForLoop.java
@@ -0,0 +1,14 @@
+package org.base;
+
+public class ForLoop {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 100; i+=2) {
+	      if (i == 0) {
+	    	  continue;
+	      }
+	      System.out.println(i);
+
+	    }
+
+	  }
+}
